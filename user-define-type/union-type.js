be0351a6c154a03951deacbc8b555132a6ddb7a0
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-09-05 15:30:31 +0600

    union type

---
+++ user-define-type/union-type.js
@@ -0,0 +1,11 @@
+// user defined data type - 
+// union data type: when user have specific data they can use it
+var userId;
+userId = "12";
+userId = true;
+userId = 12;
+function user(id) {
+    console.log(id);
+}
+user("12");
+user(12);
