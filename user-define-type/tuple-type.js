fc2d8f01d537848cb2a8096b388e6c41a6a2bf22
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-09-06 10:55:49 +0600

    add tuple

---
+++ user-define-type/tuple-type.js
@@ -0,0 +1,5 @@
+// mix data type type (vale have key, value)
+var user;
+user = [101, "zahid"];
+user.push(102, "fow kotha");
+console.log(user);
---
+++ user-define-type/tuple-type.ts
@@ -0,0 +1,9 @@
+// mix data type is tuple (vale have key, value)
+
+let user: [number,string];
+user = [101,"zahid"]
+
+user.push(102,"fow kotha")
+
+console.log(user);
+
