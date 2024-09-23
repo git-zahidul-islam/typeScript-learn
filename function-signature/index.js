67a697783fdfc156c5e94c432632794b4fe75c69
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-09-23 23:24:05 +0600

    add some new

---
+++ function-signature/index.js
@@ -0,0 +1,16 @@
+// function signature - function signature tell function type and return type
+var userInfo1;
+var userInfo2;
+userInfo1 = function () {
+    return "bdfedfe";
+};
+userInfo2 = function (name) {
+    console.log("azhdfug");
+};
+var userInfo3;
+userInfo3 = function (name, age) {
+    return "azhdfug";
+};
+userInfo3("vgf", 23);
+userInfo2("fdhaf");
+userInfo1();
